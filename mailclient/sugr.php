8eb968bdc256b942b1c4f17ea8fd46943a52adcb
ljcool2006 <51834343+ljcool2006@users.noreply.github.com>
2024-05-02 20:21:44 -0400

    how do i even figure out this format

--- mailclient/sugr.php
+++ mailclient/sugr.php
@@ -4,4 +4,4 @@ header('Content-Type: text/javascript; charset=UTF-8');
 $term = $_GET['scq'];
 ?>
 )]}'
-["<?= htmlspecialchars($term) ?>",[],{"q":"57FkYi99ISZ9cfSn1C5nhgHlLDY"}]
+["<?= htmlspecialchars($term) ?>",[["<?= htmlspecialchars($term) ?>",0,[512]]],{"q":"57FkYi99ISZ9cfSn1C5nhgHlLDY"}]
